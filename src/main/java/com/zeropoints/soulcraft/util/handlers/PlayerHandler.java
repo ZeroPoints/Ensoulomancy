213398bb3d413f0ac215d568fec6b8a8bb64d2c2
ZeroPoints <xeropoints@gmail.com>
2018-08-26 11:17:28 +1000

    Halo for flight for testing purposes

--- src/main/java/com/zeropoints/soulcraft/util/handlers/PlayerHandler.java
+++ src/main/java/com/zeropoints/soulcraft/util/handlers/PlayerHandler.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.Level;
 import org.lwjgl.opengl.GL11;
 
 import com.zeropoints.soulcraft.Main;
+import com.zeropoints.soulcraft.items.armor.Halo;
 import com.zeropoints.soulcraft.player.ISoulpool;
 import com.zeropoints.soulcraft.player.PlayerDataProvider;
 import com.zeropoints.soulcraft.renderer.entity.mobs.EntityImp;
@@ -20,13 +21,17 @@ import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.monster.EntityCreeper;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.play.server.SPacketEntity.S15PacketEntityRelMove;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.event.entity.player.PlayerSleepInBedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -65,6 +70,94 @@ public class PlayerHandler {
 		String message = String.format("You received 50 souls, you have %1$d soul.", soul.get()); 
 		player.sendMessage(new TextComponentString(message)); 
 	}
+	
+	
+	
+	
+	@SubscribeEvent
+    public void onPlayerTick(TickEvent.PlayerTickEvent event) {
+
+		//Gets this current player
+
+        EntityPlayer player = event.player;
+		
+		
+        GrantFlight(player);
+    }
+
+	
+	
+	
+	
+	
+	public static void GrantFlight(EntityPlayer player) {
+		
+		boolean hasFlight = false;
+		//Gets players items
+
+		NonNullList<ItemStack> stack = player.inventory.armorInventory;
+		//Find whether the player has a halo on for creative flight
+
+		for(int i = 0; i < stack.size(); i++) {
+			if(stack.get(i).isEmpty() || !(stack.get(i).getItem() instanceof Halo)) {
+				
+			}
+			else {
+				hasFlight = true;
+			}
+		}
+		
+		if(!hasFlight) {
+			player.capabilities.allowFlying = false;
+			return;
+		}
+		
+		if (player.world.isRemote) {
+			//GRANT FLIGHT AND SPEED
+			player.capabilities.allowFlying = true;
+			player.capabilities.setFlySpeed(0.05F + (0.05F * 5 * (float)1D));	
+			
+		}
+
+		
+	}
+	
+	
+	
+	@SubscribeEvent
+    public void onPlayerAttacked(LivingAttackEvent event) {
+		
+		if(event.getEntityLiving() instanceof EntityPlayer) {
+
+			//Gets this current player
+			EntityPlayer player = (EntityPlayer) event.getEntityLiving();
+
+			
+			//Gets players items
+			NonNullList<ItemStack> stack = player.inventory.armorInventory;
+			boolean hasFlight = false;
+
+			//Find whether the player has a halo on for creative flight
+			for(int i = 0; i < stack.size(); i++) {
+				if(stack.get(i).isEmpty() || !(stack.get(i).getItem() instanceof Halo)) {
+					
+				}
+				else {
+
+					hasFlight = true;
+				}
+			}
+			
+			//Disable fall damage if player falling
+			if(hasFlight && event.getSource().damageType.equals("fall")) {
+				event.setCanceled(true);
+				return;
+			}
+			
+		}
+		
+	}
+	
 	
 	/*
 	@SideOnly(Side.CLIENT)
