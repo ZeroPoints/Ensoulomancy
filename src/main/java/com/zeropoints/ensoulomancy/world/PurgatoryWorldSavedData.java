f36b427e383597a904efa3250e91e18fc020ac22
ZeroPoints <xeropoints@gmail.com>
2018-10-09 21:05:33 +1100

    Debugger log and pretty printed a doco. deleted progress pics

--- src/main/java/com/zeropoints/ensoulomancy/world/PurgatoryWorldSavedData.java
+++ src/main/java/com/zeropoints/ensoulomancy/world/PurgatoryWorldSavedData.java
@@ -19,35 +19,38 @@ import net.minecraftforge.common.util.Constants;
 
 public class PurgatoryWorldSavedData extends WorldSavedData {
 
-	
 	public static PurgatoryWorldSavedData GetExisting(World world) {
-		  // The IS_GLOBAL constant is there for clarity, and should be simplified into the right branch.
-		  MapStorage storage = world.getPerWorldStorage();
-		  PurgatoryWorldSavedData instance = (PurgatoryWorldSavedData) storage.getOrLoadData(PurgatoryWorldSavedData.class, Name);
+		// The IS_GLOBAL constant is there for clarity, and should be simplified into
+		// the right branch.
+		MapStorage storage = world.getPerWorldStorage();
+		PurgatoryWorldSavedData instance = (PurgatoryWorldSavedData) storage
+				.getOrLoadData(PurgatoryWorldSavedData.class, Name);
 
-		  if (instance == null) {
-		    instance = new PurgatoryWorldSavedData();
-		    storage.setData(Name, instance);
-		  }
-		  return instance;
+		if (instance == null) {
+			Main.log("Creating new world storage");
+
+			instance = new PurgatoryWorldSavedData();
+			storage.setData(Name, instance);
 		}
-	
-	
+		return instance;
+	}
+
 	/*
-	 * Save the structure data. As its a IWorldGenerated object the purgatorychunkgenerator doesnt know about it
+	 * Save the structure data. As its a IWorldGenerated object the
+	 * purgatorychunkgenerator doesnt know about it
 	 * 
 	 */
 	public Long2ObjectMap<StructureBoundingBox> spiritTempleLocations = new Long2ObjectOpenHashMap<StructureBoundingBox>();
 
-	public static final String Name =  Reference.MOD_ID + "_purgatory_world_saved_data";
-	
+	public static final String Name = Reference.MOD_ID + "_purgatory_world_saved_data";
+
 	public PurgatoryWorldSavedData(String s) {
 		super(s);
 		this.markDirty();
 		Main.log("PurgatoryWorldSavedData: INIT 1");
 
 	}
-	
+
 	public PurgatoryWorldSavedData() {
 		super(Name);
 		this.markDirty();
@@ -58,50 +61,52 @@ public class PurgatoryWorldSavedData extends WorldSavedData {
 	
 	
 	
+	/*
+	 * Could probably optimise space by storing this differently so i dont repeat the same structured data 4-8 times for each structure(as a structure can span over that many chunk borders) 
+	 * 
+	 * 
+	 * (non-Javadoc)
+	 * @see net.minecraft.world.storage.WorldSavedData#readFromNBT(net.minecraft.nbt.NBTTagCompound)
+	 */
 	@Override
 	public void readFromNBT(NBTTagCompound nbt) {
 		spiritTempleLocations.clear();
-		
+
 		NBTTagList tagList = nbt.getTagList(WorldGenSpiritTemple.Name, Constants.NBT.TAG_COMPOUND);
-	    for(int i = 0; i < tagList.tagCount(); i++) {
-	    	
-	    	NBTTagCompound metaData = tagList.getCompoundTagAt(i);
-	    	spiritTempleLocations.put(metaData.getLong("metadata_pos"), new StructureBoundingBox(metaData.getIntArray("metadata_box")));
-	    }
-		
-	    Main.log("Read Template Length: " + spiritTempleLocations.size());
-		
+		for (int i = 0; i < tagList.tagCount(); i++) {
+
+			NBTTagCompound metaData = tagList.getCompoundTagAt(i);
+			spiritTempleLocations.put(metaData.getLong("metadata_pos"),
+					new StructureBoundingBox(metaData.getIntArray("metadata_box")));
+		}
+
+		Main.log("Read Template Length: " + spiritTempleLocations.size());
+
 	}
 
-	
-	
 	@Override
 	public NBTTagCompound writeToNBT(NBTTagCompound compound) {
-		
 
-	    Main.log("Write Template Length: " + spiritTempleLocations.size());
-	    
-	    NBTTagList tagList = new NBTTagList();
+		Main.log("Write Template Length: " + spiritTempleLocations.size());
 
-	    Iterator<?> it = spiritTempleLocations.entrySet().iterator();
-	    while (it.hasNext()) {
-	        Map.Entry pair = (Map.Entry)it.next();
-	        
-        	NBTTagCompound metaData = new NBTTagCompound();
-        	metaData.setLong("metadata_pos", (long) pair.getKey());
-        	metaData.setIntArray("metadata_box",  ((StructureBoundingBox)pair.getValue()).toNBTTagIntArray().getIntArray());
-        	tagList.appendTag(metaData);
-        	
-        	//it.remove(); // avoids a ConcurrentModificationException
-	    }
-	    
-	    compound.setTag(WorldGenSpiritTemple.Name, tagList);
-		
+		NBTTagList tagList = new NBTTagList();
+
+		Iterator<?> it = spiritTempleLocations.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry pair = (Map.Entry) it.next();
+
+			NBTTagCompound metaData = new NBTTagCompound();
+			metaData.setLong("metadata_pos", (long) pair.getKey());
+			metaData.setIntArray("metadata_box",
+					((StructureBoundingBox) pair.getValue()).toNBTTagIntArray().getIntArray());
+			tagList.appendTag(metaData);
+
+			// it.remove(); // avoids a ConcurrentModificationException
+		}
+
+		compound.setTag(WorldGenSpiritTemple.Name, tagList);
+
 		return compound;
 	}
 
-	
-	
-	
-	
 }
--- src/main/java/com/zeropoints/ensoulomancy/world/gen/structure/WorldGenSpiritTemple.java
+++ src/main/java/com/zeropoints/ensoulomancy/world/gen/structure/WorldGenSpiritTemple.java
@@ -29,180 +29,168 @@ import net.minecraftforge.fml.common.IWorldGenerator;
 
 public class WorldGenSpiritTemple implements IWorldGenerator {
 
-    private static final ResourceLocation SPIRIT_TEMPLE = new ResourceLocation(Reference.MOD_ID + ":spirit_temple_centered");
+	private static final ResourceLocation SPIRIT_TEMPLE = new ResourceLocation(
+			Reference.MOD_ID + ":spirit_temple_centered");
 
-    public static final String Name = Reference.MOD_ID + "_spirit_temple";
-    
+	public static final String Name = Reference.MOD_ID + "_spirit_temple";
 
-    public static List<Biome.SpawnListEntry> SpawnableMonsterList = Lists.<Biome.SpawnListEntry>newArrayList(
-    		new SpawnListEntry(EntityPixie.class, 1, 1, 1)
-    		);
+	public static List<Biome.SpawnListEntry> SpawnableMonsterList = Lists.<Biome.SpawnListEntry>newArrayList(
+			new SpawnListEntry(EntityPixie.class, 1, 1, 1));
 
-    
-    public static List<Biome.SpawnListEntry> getSpawnableList(EnumCreatureType creatureType)
-    {
-        switch (creatureType)
-        {
-            case MONSTER:
-                return SpawnableMonsterList;
-            case CREATURE:
-                return null;
-            case WATER_CREATURE:
-                return null;
-            case AMBIENT:
-                return null;
-            default:
-            	return null;
-        }
-    }
-    
-    
-    
-    
+	public static List<Biome.SpawnListEntry> getSpawnableList(EnumCreatureType creatureType) {
+		switch (creatureType) {
+		case MONSTER:
+			return SpawnableMonsterList;
+		case CREATURE:
+			return null;
+		case WATER_CREATURE:
+			return null;
+		case AMBIENT:
+			return null;
+		default:
+			return null;
+		}
+	}
+
 	@Override
-	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
-		
-		
-		
-		
-		if(world.provider.getDimension() != ConfigurationHandler.dimensionId) {
+	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator,
+			IChunkProvider chunkProvider) {
+
+		if (world.provider.getDimension() != ConfigurationHandler.dimensionId) {
 			return;
 		}
-	
-        if(canSpawnStructureAtCoords(random, world, chunkX, chunkZ)) {
-        	//Random block in that chunks 16x16 square
-        	int x = chunkX * 16 + random.nextInt(4)+6;
-        	int z = chunkZ * 16 + random.nextInt(4)+6;
-        	
-    		BlockPos basePos = new BlockPos(x, world.getHeight(x, z), z);
-    		Biome currentBiome = world.getBiome(basePos);
-    		//Biome is  spirit biome only
-    		if(currentBiome.getRegistryName() != ModBiomes.SPIRIT_BIOME.getRegistryName()) {
-    			return;
-    		}
-    		
-			
-    		//Random rotation...
+
+		if (canSpawnStructureAtCoords(random, world, chunkX, chunkZ)) {
+			// Random block in that chunks 16x16 square
+			int x = chunkX * 16 + random.nextInt(16);
+			int z = chunkZ * 16 + random.nextInt(16);
+
+			BlockPos basePos = new BlockPos(x, world.getHeight(x, z), z);
+			Biome currentBiome = world.getBiome(basePos);
+			// Biome is spirit biome only
+			if (currentBiome.getRegistryName() != ModBiomes.SPIRIT_BIOME.getRegistryName()) {
+				return;
+			}
+
+			// Random rotation...
 			Rotation rot = Rotation.values()[random.nextInt(Rotation.values().length)];
-            final PlacementSettings settings = new PlacementSettings().setRotation(rot);        
-            final Template template = world.getSaveHandler().getStructureTemplateManager().getTemplate(world.getMinecraftServer(), SPIRIT_TEMPLE);
-            
-            //Structure Size
-            BlockPos structureSize = template.transformedSize(rot);
-            
-            
-            //4 Corners
-            BlockPos oppCornerTransformedOffset = Template.transformedBlockPos(settings, structureSize);
-            BlockPos oppCorner = basePos.add(oppCornerTransformedOffset);
-            
-            //Reget the height for the center block. Otherwise its always corner origin...
-            int centerX = x = x+(oppCornerTransformedOffset.getX()/2);
-            int centerZ = z+(oppCornerTransformedOffset.getZ()/2);
-            basePos = new BlockPos(x, world.getHeight(centerX, centerZ), z);
-            	
-            
-            if(basePos.getY() < ((ICustomBiome)currentBiome).GetMinHeight()) {
-            	Main.log("Spirit temple spawning way to low for biome");
-            	return;
-            }
-            
-            
-    		StructureBoundingBox boundingBox = new StructureBoundingBox(basePos.getX(), basePos.getY(), basePos.getZ(), oppCorner.getX(), oppCorner.getY(), oppCorner.getZ());
-            
-    		
-            
-    		Main.log("SpiritTemple Generated: " + basePos.toString() + ", rotation: " + rot.toString() + ", oppCorner: " + oppCorner.toString() + ", structSize: " + structureSize.toString() + ", boundBox: " + boundingBox);
-            template.addBlocksToWorld(world, basePos, settings);
-            
-           
-            //4 Corners as our structure at max can overflow to 4 chunks
-            long chunkPos1 = ChunkPos.asLong(basePos.getX()/16, basePos.getZ()/16);
-            long chunkPos2 = ChunkPos.asLong(oppCorner.getX()/16, oppCorner.getZ()/16);            
+			final PlacementSettings settings = new PlacementSettings().setRotation(rot);
+			final Template template = world.getSaveHandler().getStructureTemplateManager()
+					.getTemplate(world.getMinecraftServer(), SPIRIT_TEMPLE);
 
-            BlockPos otherCorner3 = basePos.add(oppCornerTransformedOffset.getX(), 0, 0);
-            long chunkPos3 = ChunkPos.asLong(otherCorner3.getX()/16, otherCorner3.getZ()/16);
+			// Structure Size
+			BlockPos structureSize = template.transformedSize(rot);
 
-            BlockPos otherCorner4 = basePos.add(0, 0, oppCornerTransformedOffset.getZ());
-            long chunkPos4 = ChunkPos.asLong(otherCorner4.getX()/16, otherCorner4.getZ()/16);
-            
+			// 4 Corners
+			BlockPos oppCornerTransformedOffset = Template.transformedBlockPos(settings, structureSize);
+			BlockPos oppCorner = basePos.add(oppCornerTransformedOffset);
 
-            //Lookup worldsaveddata. We do this so we can lookup in mobspawning. So we can spawn our mob type inside bounding box area of structure
-            
-            
-            /*
-            PurgatoryChunkGenerator.purgatoryWorldSavedData.spiritTempleLocations.put(chunkPos1, boundingBox);      
-            
-            PurgatoryChunkGenerator.purgatoryWorldSavedData.spiritTempleLocations.put(chunkPos2, boundingBox);
-            
-            PurgatoryChunkGenerator.purgatoryWorldSavedData.spiritTempleLocations.put(chunkPos3, boundingBox);   
-            
-            PurgatoryChunkGenerator.purgatoryWorldSavedData.spiritTempleLocations.put(chunkPos4, boundingBox);
-            
-            PurgatoryChunkGenerator.purgatoryWorldSavedData.markDirty();
-            */
-            
-            
-            /*
-            ((PurgatoryChunkGenerator)chunkGenerator).purgatoryWorldSavedData.spiritTempleLocations.put(chunkPos1, boundingBox);      
-            
-            ((PurgatoryChunkGenerator)chunkGenerator).purgatoryWorldSavedData.spiritTempleLocations.put(chunkPos2, boundingBox);
-            
-            ((PurgatoryChunkGenerator)chunkGenerator).purgatoryWorldSavedData.spiritTempleLocations.put(chunkPos3, boundingBox);   
-            
-            ((PurgatoryChunkGenerator)chunkGenerator).purgatoryWorldSavedData.spiritTempleLocations.put(chunkPos4, boundingBox);
-            
-            ((PurgatoryChunkGenerator)chunkGenerator).purgatoryWorldSavedData.markDirty();
-            */
-            
+			// Reget the height for the center block. Otherwise its always corner origin...
+			int centerX = x = x + (oppCornerTransformedOffset.getX() / 2);
+			int centerZ = z + (oppCornerTransformedOffset.getZ() / 2);
+			basePos = new BlockPos(x, world.getHeight(centerX, centerZ), z);
 
-            PurgatoryWorldSavedData worldData = PurgatoryWorldSavedData.GetExisting(world);
-            worldData.spiritTempleLocations.put(chunkPos1, boundingBox);      
-            
-            worldData.spiritTempleLocations.put(chunkPos2, boundingBox);
-            
-            worldData.spiritTempleLocations.put(chunkPos3, boundingBox);   
-            
-            worldData.spiritTempleLocations.put(chunkPos4, boundingBox);
-            
-            worldData.markDirty();
-            
-        }
-        
-        
+			if (basePos.getY() < ((ICustomBiome) currentBiome).GetMinHeight()) {
+				Main.log("Spirit temple spawning way to low for biome");// Could write something hacky to reget top
+																		// block by scanning from maxheight to
+																		// minheight.
+				return;
+			}
+
+			StructureBoundingBox boundingBox = new StructureBoundingBox(basePos.getX(), basePos.getY(), basePos.getZ(),
+					oppCorner.getX(), oppCorner.getY(), oppCorner.getZ());
+
+			Main.log("SpiritTemple Generated: " + basePos.toString() + ", rotation: " + rot.toString() + ", oppCorner: "
+					+ oppCorner.toString() + ", structSize: " + structureSize.toString() + ", boundBox: "
+					+ boundingBox);
+			template.addBlocksToWorld(world, basePos, settings);
+
+			// 4 Corners as our structure at max can overflow to 4 chunks
+			long chunkPos1 = ChunkPos.asLong(basePos.getX() / 16, basePos.getZ() / 16);
+			long chunkPos2 = ChunkPos.asLong(oppCorner.getX() / 16, oppCorner.getZ() / 16);
+
+			BlockPos otherCorner3 = basePos.add(oppCornerTransformedOffset.getX(), 0, 0);
+			long chunkPos3 = ChunkPos.asLong(otherCorner3.getX() / 16, otherCorner3.getZ() / 16);
+
+			BlockPos otherCorner4 = basePos.add(0, 0, oppCornerTransformedOffset.getZ());
+			long chunkPos4 = ChunkPos.asLong(otherCorner4.getX() / 16, otherCorner4.getZ() / 16);
+
+			// Lookup worldsaveddata. We do this so we can lookup in mobspawning. So we can
+			// spawn our mob type inside bounding box area of structure
+
+			/*
+			 * PurgatoryChunkGenerator.purgatoryWorldSavedData.spiritTempleLocations.put(
+			 * chunkPos1, boundingBox);
+			 * 
+			 * PurgatoryChunkGenerator.purgatoryWorldSavedData.spiritTempleLocations.put(
+			 * chunkPos2, boundingBox);
+			 * 
+			 * PurgatoryChunkGenerator.purgatoryWorldSavedData.spiritTempleLocations.put(
+			 * chunkPos3, boundingBox);
+			 * 
+			 * PurgatoryChunkGenerator.purgatoryWorldSavedData.spiritTempleLocations.put(
+			 * chunkPos4, boundingBox);
+			 * 
+			 * PurgatoryChunkGenerator.purgatoryWorldSavedData.markDirty();
+			 */
+
+			/*
+			 * ((PurgatoryChunkGenerator)chunkGenerator).purgatoryWorldSavedData.
+			 * spiritTempleLocations.put(chunkPos1, boundingBox);
+			 * 
+			 * ((PurgatoryChunkGenerator)chunkGenerator).purgatoryWorldSavedData.
+			 * spiritTempleLocations.put(chunkPos2, boundingBox);
+			 * 
+			 * ((PurgatoryChunkGenerator)chunkGenerator).purgatoryWorldSavedData.
+			 * spiritTempleLocations.put(chunkPos3, boundingBox);
+			 * 
+			 * ((PurgatoryChunkGenerator)chunkGenerator).purgatoryWorldSavedData.
+			 * spiritTempleLocations.put(chunkPos4, boundingBox);
+			 * 
+			 * ((PurgatoryChunkGenerator)chunkGenerator).purgatoryWorldSavedData.markDirty()
+			 * ;
+			 */
+
+			PurgatoryWorldSavedData worldData = PurgatoryWorldSavedData.GetExisting(world);
+			worldData.spiritTempleLocations.put(chunkPos1, boundingBox);
+
+			worldData.spiritTempleLocations.put(chunkPos2, boundingBox);
+
+			worldData.spiritTempleLocations.put(chunkPos3, boundingBox);
+
+			worldData.spiritTempleLocations.put(chunkPos4, boundingBox);
+
+			worldData.markDirty();
+
+		}
+
 	}
-	
-	
 
-	
-	
 	/*
 	 * Found in end structure code.
 	 */
-	private boolean canSpawnStructureAtCoords(Random random, World world, int chunkX, int chunkZ)
-    {
-        int i = chunkX;
-        int j = chunkZ;
+	private boolean canSpawnStructureAtCoords(Random random, World world, int chunkX, int chunkZ) {
+		int i = chunkX;
+		int j = chunkZ;
 
-        if (chunkX < 0)
-        {
-            chunkX -= 19;
-        }
+		if (chunkX < 0) {
+			chunkX -= 19;
+		}
 
-        if (chunkZ < 0)
-        {
-            chunkZ -= 19;
-        }
+		if (chunkZ < 0) {
+			chunkZ -= 19;
+		}
 
-        int k = chunkX / 20;
-        int l = chunkZ / 20;
-        random = world.setRandomSeed(k, l, 10387313);
-        k = k * 20;
-        l = l * 20;
-        k = k + (random.nextInt(9) + random.nextInt(9)) / 2;
-        l = l + (random.nextInt(9) + random.nextInt(9)) / 2;
+		int k = chunkX / 20;
+		int l = chunkZ / 20;
+		random = world.setRandomSeed(k, l, 10387313);
+		k = k * 20;
+		l = l * 20;
+		k = k + (random.nextInt(9) + random.nextInt(9)) / 2;
+		l = l + (random.nextInt(9) + random.nextInt(9)) / 2;
 
-        return (i == k && j == l);
-       
-    }
-	
+		return (i == k && j == l);
+
+	}
+
 }
